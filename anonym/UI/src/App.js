aba2570687e121281781be87a38b39dcbbf7cf09
Jahlil Walker <jgw298@nyu.edu>
2020-04-17 00:50:58 -0400

    Layout changes

--- anonym/UI/src/App.js
+++ anonym/UI/src/App.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import logo from './priv.png';
+import { Jumbotron } from 'reactstrap';
+import { InputGroup, InputGroupText, InputGroupAddon, Input } from 'reactstrap';
+import proPic from './silhoe.jpg';
 import './App.css';
-import Form from './Form'
+
+const Header = () => {
+  return (
+    <Jumbotron>
+      <h1 className="display-5">Welcome to Anonym, type below to start the conversation</h1>
+      <p className="lead">The completely anonymous police report compiler.</p>
+    </Jumbotron>
+  )
+}
+
+
+const Message = (prop) => {
+  return(
+    <div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo}/>
-        <p>
-          Welcome to Anonym.
-        </p>
-        <h1>
-          Bruh
-        </h1>
-        <Form/>
-        
-      </header>
+    <div className="mainBody">
+      <Header/>
+        <img src={proPic} className="proPic" />
+        <p className="chatMessage">Hello how are you doing?</p>    
+        <br></br>  
+        <p className="replyMessage">I just witnessed a crime and would like to report it</p>   
+        <InputGroup className='textBox'>
+          <Input />
+          <InputGroupAddon addonType="append">
+            <InputGroupText>Send</InputGroupText>
+          </InputGroupAddon>
+        </InputGroup>
     </div>
   );
 }
