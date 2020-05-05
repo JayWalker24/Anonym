0b6dee46800fa69855415c73a94a63bbd50172b1
Asim Satti <aks618@nyu.edu>
2020-05-05 05:38:48 -0400

    Verbose Logic

--- formFilling/venv/ReportEvaluation/ANONYMReport.py
+++ formFilling/venv/ReportEvaluation/ANONYMReport.py
@@ -3,6 +3,8 @@ import datetime
 
 from ANONYMLocation import ReportLocation
 
+import TestDuplicates as TD
+
 class ANONYMReport:
     
     def __init__(self,jsonfile=None):
@@ -25,10 +27,12 @@ class ANONYMReport:
         
         if(jsonfile!=None):
             self.readJson(jsonfile)
+            self.checkLogic()
             
         
     def setJsonFile(self,jsonfile):
         self.readJson(jsonfile)
+        self.checkLogic()
 
     def CheckDateTime(self):
     
@@ -39,7 +43,12 @@ class ANONYMReport:
         if(self.dateofIncident==self.dateofReport and self.timeofReport<self.timeofIncident):
                 Message ="ERROR!! Impossible, the time of the crime is newer than the time of the report"
                 self.errorHandler(Message)
-
+                
+    def checkLogic(self):
+            #Checking logicality
+            self.CheckDateTime()
+            self.CheckLocation()
+           #self.isDuplicate()
         
     def CheckLocation(self):
        #Address from API
@@ -50,60 +59,70 @@ class ANONYMReport:
            Message = "Warning is this the address: "+ reportedAddress
            self.errorHandler(Message)
 
-       
+    def NormalizeLocation(self,DB_location):
+        rdistance = TD.Noself.locService.DistanceFrom(DB_location)
+        adjustedLoc = float(1 - (rdistance/1000))
+        LocationGrade =0
+        if(adjustedLoc<0):
+            LocationGrade = 0
+        else:
+            LocationGrade = adjustedLoc
+        return LocationGrade/2
+         
+    def isDuplicate(self):
+        ## for i in reports submitted
+        message=""
+        DB_location, DB_time, DB_description = TD.readJson("report2.json")
+        TextGrade = TD.TextGrader((self.crimeDescription+" "+self.suspectDescription), DB_description)
+        LocationGrade = TD.NormalizeLocation(self.locService.DistanceFrom(DB_location))
+        print(TextGrade,LocationGrade)
+        if(TextGrade+LocationGrade>0.80):
+            message = "A Likely Duplicate"
+            self.errorHandler(message)
+                    
+    def timeHandler(self,time):
+     
+        colon = time.index(":")
+        hr = int(time[:colon])
+        minutes =int(time[colon+1:colon+3])
+        
+        if("PM" in time or "pm" in time):
+            hr = (hr+12)%24
+        return datetime.time(hr,minutes)
+
+    def dateHandler(self,date):
+        date = date.replace("/"," ")
+        div = date.index(" ")
+        month = int(date[:div])
+        day = int(date[div+1:div+3])
+        year = int(date[-4:])
+      
+        return datetime.date(year,month,day)
     def readJson(self,jsonfile):
             
             file= open(jsonfile,)
             data = json.load(file)
             for i in data:
                 if(i=="date_crime"):
-                    date = data[i].replace("/"," ")
-                    month = int(date[:2])
-                    day = int(date[2:4])
-                    year = int(date[-4:])
-                    self.dateofIncident=datetime.date(year,month,day)
+                    self.dateofIncident=self.dateHandler(data[i])
                 elif(i=="time_crime"):
-                    time= data[i].replace(":"," ")
-                    hour=int(time[:2])
-                    minute=int(time[3:5])
-                    second=int(time[6:])
-                    self.timeofIncident = datetime.time(hour,minute,second)
+                    self.timeofIncident = self.timeHandler(data[i])
                 elif(i=="crime_description"):
                     self.crimeDescription = data[i]
-                elif(i=="suspect_desctiption"):
+                elif(i=="suspect_description"):
                     self.suspectDescription = data[i]
                 elif(i=="date_report"):
-                    date = data[i].replace("/"," ")
-                    month = int(date[:2])
-                    day = int(date[2:4])
-                    year = int(date[-4:])
-                    self.dateofReport=datetime.date(year,month,day)
-                    
+                    self.dateofReport=self.dateHandler(data[i])           
                 elif(i=="time_report"):
-                    time= data[i].replace(":"," ")
-                    hour=int(time[:2])
-                    minute=int(time[3:5])
-                    second=int(time[6:])
-                    self.timeofReport = datetime.time(hour,minute,second)
+                    self.timeofReport = self.timeHandler(data[i])
                 elif(i=="location_crime"):
                     self.locationofIncident = data[i]
             file.close()
-
-            #Checking logicality
-            self.CheckDateTime()
-            self.CheckLocation()
             
     def errorHandler(self,message):
         print(message)
 
-def main():
-    #Pass a json file ANONYM does time and location checks
-    #AND PRints errors and warning
-    JsonReport="report.json"
-
-    
-    newReport = ANONYMReport(JsonReport)
  
-        
-
-main()
+    
+#  newReport = ANONYMReport(JsonReport)
+ 
